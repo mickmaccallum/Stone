1d442e655d2f27c106e63273fd06a254def33c47
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-07 23:55:21 -0400

    Added Xcode project.

---
+++ Phoenix/Phoenix/Phoenix.h
@@ -0,0 +1,19 @@
+//
+//  Phoenix.h
+//  Phoenix
+//
+//  Created by Michael MacCallum on 5/7/16.
+//  Copyright © 2016 Tethr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Phoenix.
+FOUNDATION_EXPORT double PhoenixVersionNumber;
+
+//! Project version string for Phoenix.
+FOUNDATION_EXPORT const unsigned char PhoenixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Phoenix/PublicHeader.h>
+
+
