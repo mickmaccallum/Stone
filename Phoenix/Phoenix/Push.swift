492dcc0edb6eafe2d3f9bebf2a31a5526b8df9a8
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-17 02:21:01 -0400

    Linked Wrap for JSON encoding

--- Phoenix/Phoenix/Push.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Push.swift
-//  Phoenix
-//
-//  Created by Michael MacCallum on 5/16/16.
-//  Copyright © 2016 Tethr. All rights reserved.
-//
-
-import Foundation
---
+++ Phoenix/Phoenix/QueryStringConvertible.swift
@@ -0,0 +1,14 @@
+//
+//  QueryStringConvertible.swift
+//  Phoenix
+//
+//  Created by Michael MacCallum on 5/16/16.
+//  Copyright © 2016 Tethr Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol QueryStringConvertible: Hashable {
+	var queryStringRepresentation: String { get }
+}
+
