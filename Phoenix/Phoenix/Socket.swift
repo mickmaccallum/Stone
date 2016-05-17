e3b61acca7c0fa188ee15af49978936e505a19c1
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-17 03:27:46 -0400

    Started implementing Socket

--- Phoenix/Phoenix/Socket.swift
+++ Phoenix/Phoenix/Socket.swift
@@ -9,8 +9,41 @@
 import Foundation
 import SwiftWebSocket
 
+
+public typealias SocketState = WebSocketReadyState
+
 public final class Socket {
-	
+	private var socket: WebSocket?
+	public let heartbeatInterval: NSTimeInterval
+	public let url: NSURL
+	private var channels = Set<Channel>()
+	private let queue = NSOperationQueue()
+
+	public init(url: NSURL, heartbeatInterval: NSTimeInterval = 0.0) {
+		self.url = url
+		self.heartbeatInterval = heartbeatInterval
+
+		queue.suspended = true
+	}
+
+	public func connect<T: QueryStringConvertible>(params: [T: T]) {
+		connect(params.toQueryItems())
+	}
+
+	public func connect(params: [NSURLQueryItem]? = nil) {
+		socket = WebSocket(url: url.urlByAppendingQueryItems(params) ?? url)
+		socket?.delegate = self
+		socket?.open()
+		socket?.readyState
+	}
+
+	public func addChannel(channel: Channel) {
+		channels.insert(channel)
+	}
+
+	public func removeChannel(channel: Channel) -> Channel? {
+		return channels.remove(channel)
+	}
 }
 
 extension Socket: WebSocketDelegate {
