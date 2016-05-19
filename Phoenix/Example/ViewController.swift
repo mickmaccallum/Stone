8266c0f85a974f0497d16dcca1b36ff94e76a2e0
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-19 18:32:51 -0400

    trimming

--- Phoenix/Example/ViewController.swift
+++ Phoenix/Example/ViewController.swift
@@ -8,70 +8,47 @@
 
 import UIKit
 import Phoenix
-import SwiftWebSocket
 
-struct Message {
-	let topic: String
-	let event: String
-	let ref: String
-	let subject: String
-	let body: String
-
-	func toJsonString() -> String {
-		return "{" +
-		"\"topic\":\"\(topic)\"" +
-		"\"event\":\"\(event)\"" +
-		"\"ref\":\"\(ref)\"" +
-		"\"subject\":\"\(subject)\"" +
-		"\"body\":\"\(body)\"" +
-		"}"
-	}
-}
 
 class ViewController: UIViewController {
-	var socket: WebSocket!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		let url = NSURL(string: "ws://localhost:4000/socket/websocket?user_id=mick")!
+		let params = [NSURLQueryItem(name: "user_id", value: "mick")]
 
-		let request = NSMutableURLRequest(
-			URL: url,
-			cachePolicy: .ReloadIgnoringLocalAndRemoteCacheData,
-			timeoutInterval: 15.0
+		let url = NSURL(string: "ws://localhost:4000/socket")!
+
+		let socket = Socket(
+			url: url,
+			heartbeatInterval: 15.0
 		)
 
-		request.HTTPBody = "{\"dummy\":\"data\"".dataUsingEncoding(NSUTF8StringEncoding)
-
-		let ws = WebSocket(request: request)
-
-		let send : ()->() = {
-			let msg = "{\"topic\": \"rooms:lobby\", \"event\": \"phx_join\", \"ref\": \"1\", \"payload\": {\"subject\":\"status\",\"body\":\"joining\"}}"
-			print("send:\(msg)")
-			ws.send(msg)
-		}
-		ws.event.open = {
-			print("opened")
-			send()
-		}
-		ws.event.close = { code, reason, clean in
-			print("closed for reason: \(reason) ---- clean: \(clean)")
-		}
-		ws.event.error = { error in
-			print("error \(error)")
-		}
-		ws.event.message = { message in
-			if let text = message as? String {
-				print("recv: \(text)")
-//				if messageNum == 10 {
-//					ws.close()
-//				} else {
-////					send()
-//				}
-			}
+		socket.onSocketOpen = {
+			print("socket open")
 		}
 
+		socket.onSocketMessage = { result in
+			print("received message: \(result)")
+		}
+
+		socket.onSocketError = { error in
+			print("error received: \(error)")
+		}
+
+		socket.onSocketClose = { code, reason, clean in
+			print("socket closed - code: \(code), reason: \(reason), clean: \(clean)")
+		}
+
+		socket.connect(params)
+
+		let channel = Channel(socket: socket, topic: "rooms:lobby")
+
+		channel.onEvent(Event.Default(.Join)) { (result) in
+			print("on channel join: \(result)")
+		}
+
+//		channel.onEvent(<#T##event: RawType##RawType#>, callback: <#T##Callback##Callback##Result<Void> -> Void#>)
 
 
 		// Do any additional setup after loading the view, typically from a nib.
@@ -85,32 +62,3 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: WebSocketDelegate {
-	func webSocketOpen() {
-		print("open")
-	}
-
-	func webSocketPong() {
-		print("pong")
-	}
-
-	func webSocketError(error: NSError) {
-		print(error)
-	}
-
-	func webSocketMessageText(text: String) {
-		print("got text: \(text)")
-	}
-
-	func webSocketMessageData(data: NSData) {
-		print("got data: \(data)")
-	}
-
-	func webSocketClose(code: Int, reason: String, wasClean: Bool) {
-		print("closed: \(reason)")
-	}
-
-	func webSocketEnd(code: Int, reason: String, wasClean: Bool, error: NSError?) {
-		print("ended: \(reason)")
-	}
-}
