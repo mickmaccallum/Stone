97f43cf2bc280d1e32a2ed752ba1c8d4867e1eea
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-19 17:01:24 -0400

    Event is pretty much done

--- Phoenix/Phoenix/Event.swift
+++ Phoenix/Phoenix/Event.swift
@@ -7,11 +7,52 @@
 //
 
 import Foundation
+import Unbox
+import Wrap
 
-public enum Event: String {
-	case Join = "phx_join"
-	case Reply = "phx_reply"
-	case Leave = "phx_leave"
-	case Close = "phx_close"
-	case Error = "phx_error"
+public enum Event: RawRepresentable {
+	public enum PhoenixEvent: String {
+		case Join		= "phx_join"
+		case Reply		= "phx_reply"
+		case Leave		= "phx_leave"
+		case Close		= "phx_close"
+		case Error		= "phx_error"
+		case Heartbeat	= "heartbeat"
+	}
+
+	case Default(PhoenixEvent)
+	case Custom(String)
+
+	public var rawValue: String {
+		switch self {
+		case .Default(let known):
+			return known.rawValue
+		case .Custom(let str):
+			return str
+		}
+	}
+
+	public init?(rawValue: String) {
+		if let def = PhoenixEvent(rawValue: rawValue) {
+			self = .Default(def)
+		} else {
+			self = .Custom(rawValue)
+		}
+	}
+}
+
+extension Event: UnboxableRawType {
+	public static func unboxFallbackValue() -> Event {
+		return .Custom("")
+	}
+
+	public static func transformUnboxedString(unboxedString: String) -> Event? {
+		return Event(rawValue: unboxedString)
+	}
+}
+
+extension Event: WrappableEnum {
+	public func wrap() -> AnyObject? {
+		return rawValue
+	}
 }
