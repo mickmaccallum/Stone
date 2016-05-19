51fe43de8a873096cece442164b5c299f680fe37
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-19 13:50:23 -0400

    Added a lot of basic functionality

--- Phoenix/Phoenix/Socket.swift
+++ Phoenix/Phoenix/Socket.swift
@@ -8,16 +8,25 @@
 
 import Foundation
 import SwiftWebSocket
-
+import Wrap
 
 public typealias SocketState = WebSocketReadyState
 
 public final class Socket {
-	private var socket: WebSocket?
+	public private(set) var socket: WebSocket?
 	public let heartbeatInterval: NSTimeInterval
 	public let url: NSURL
+	public var reconnectOnError = true
+
+	private var heartBeatTimer: NSTimer?
+	private var reconnectTimer: NSTimer?
 	private var channels = Set<Channel>()
+	private var lastParams: [NSURLQueryItem]?
 	private let queue = NSOperationQueue()
+
+	public var onSocketOpen: (() -> Void)?
+	public var onSocketError: ((code: Int, reason: String, wasClean: Bool, error: NSError?) -> Void)?
+	public var onSocketClose: ((code: Int, reason: String, wasClean: Bool) -> Void)?
 
 	public init(url: NSURL, heartbeatInterval: NSTimeInterval = 0.0) {
 		self.url = url
@@ -31,10 +40,32 @@ public final class Socket {
 	}
 
 	public func connect(params: [NSURLQueryItem]? = nil) {
+		lastParams = params
 		socket = WebSocket(url: url.urlByAppendingQueryItems(params) ?? url)
 		socket?.delegate = self
 		socket?.open()
 		socket?.readyState
+	}
+
+	public func reconnect() {
+		disconnectSocket()
+		connect(lastParams)
+	}
+
+	public func disconnect() {
+		discardHeartBeatTimer()
+		discardReconnectTimer()
+		disconnectSocket()
+	}
+
+	private func disconnectSocket() {
+		guard let socket = socket else {
+			return
+		}
+
+		socket.delegate = nil
+		socket.close()
+		self.socket = nil
 	}
 
 	public func addChannel(channel: Channel) {
@@ -44,11 +75,63 @@ public final class Socket {
 	public func removeChannel(channel: Channel) -> Channel? {
 		return channels.remove(channel)
 	}
+
+	private func startHeatBeatTimer() {
+		discardHeartBeatTimer()
+
+		heartBeatTimer = NSTimer.scheduledTimerWithTimeInterval(
+			heartbeatInterval,
+			target: self,
+			selector: #selector(Socket.sendHeartBeat),
+			userInfo: nil,
+			repeats: true
+		)
+	}
+
+	@objc private func sendHeartBeat() {
+		let message = Message(
+			topic: "phoenix",
+			event: "heartbeat"
+		)
+
+		try! push(message)
+	}
+
+	private func discardHeartBeatTimer() {
+		if let timer = heartBeatTimer where timer.valid {
+			timer.invalidate()
+		}
+
+		heartBeatTimer = nil
+	}
+
+	private func discardReconnectTimer() {
+		if let timer = reconnectTimer where timer.valid {
+			timer.invalidate()
+		}
+
+		reconnectTimer = nil
+	}
+
+	func push(message: Message) throws {
+		let dict: [String: AnyObject] = try Wrap(message)
+		let jsonData = try NSJSONSerialization.dataWithJSONObject(dict, options: [])
+
+		queue.addOperationWithBlock { [weak self] in
+			self?.socket?.send(jsonData)
+		}
+	}
 }
 
 extension Socket: WebSocketDelegate {
 	@objc public func webSocketOpen() {
+		queue.suspended = false
 
+		if heartbeatInterval > 0 {
+			startHeatBeatTimer()
+		}
+
+		onSocketOpen?()
 	}
 
 	@objc public func webSocketPong() {
@@ -68,10 +151,11 @@ extension Socket: WebSocketDelegate {
 	}
 
 	@objc public func webSocketClose(code: Int, reason: String, wasClean: Bool) {
-
+		onSocketClose?(code: code, reason: reason, wasClean: wasClean)
 	}
 
 	@objc public func webSocketEnd(code: Int, reason: String, wasClean: Bool, error: NSError?) {
-
+		// close and error handled here.
+//		onSocketError?(code: code, reason: reason, wasClean: wasClean, error: error)
 	}
 }
