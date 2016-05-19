1a21c95a8dcb7ba6fd6c29bbbff30c76131f437b
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-19 17:02:06 -0400

    cleanup

--- Phoenix/Phoenix/Channel.swift
+++ Phoenix/Phoenix/Channel.swift
@@ -14,16 +14,30 @@ public enum ChannelState {
 }
 
 public final class Channel: Hashable, Equatable {
+	public let topic: String
+
 	public typealias Callback = Result<Void> -> Void
 	public private(set) var state: ChannelState = .Closed
 	private var bindings = [String: Callback]()
 
-	public init(socket: Socket) {
+	public convenience init<RawType: RawRepresentable where RawType.RawValue == String>(socket: Socket, topic: RawType) {
+		self.init(socket: socket, topic: topic.rawValue)
+	}
 
+	public init(socket: Socket, topic: String) {
+		self.topic = topic
 	}
 
 	public var hashValue: Int {
-		return 0
+		return topic.hashValue
+	}
+
+	internal func sendMessage(message: Message) {
+		
+	}
+
+	internal func triggerEvent(event: Event, ref: String, payload: [String: AnyObject]? = nil) {
+
 	}
 
 	public func onEvent<RawType: RawRepresentable where RawType.RawValue == String>(event: RawType, callback: Callback) {
@@ -31,7 +45,7 @@ public final class Channel: Hashable, Equatable {
 	}
 
 	public func onEvent(event: String, callback: Callback) {
-
+		
 	}
 }
 
