e3b4394ce0f4a7c791ea236c34cffff4adf05d7e
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-20 02:46:08 -0400

    Added ChannelState

---
+++ Stone/Stone/ChannelState.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelState.swift
+//  Stone
+//
+//  Created by Michael MacCallum on 5/20/16.
+//  Copyright © 2016 Tethr. All rights reserved.
+//
+
+import Foundation
+
+/**
+Indicates the current connection status of a Channel.
+
+- Closed:	Indicates that the Channel is closed.
+- Errored:	Indicates that the Channel is closed with an error.
+- Joining:	Indicates that the Channel is currently trying to join.
+- Joined:	Indicates that the Channel is joined and active.
+*/
+public enum ChannelState {
+	case Closed, Errored, Joining, Joined
+}
