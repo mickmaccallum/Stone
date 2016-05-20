6b7bc0a2911a4e24dae4bd7c2d122fc9058136ff
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-20 01:00:31 -0400

    Added joining and event handling

---
+++ Stone/Error.swift
@@ -0,0 +1,9 @@
+//
+//  Error.swift
+//  Stone
+//
+//  Created by Michael MacCallum on 5/19/16.
+//  Copyright © 2016 Tethr. All rights reserved.
+//
+
+import Foundation
--- Stone/Stone/Channel.swift
+++ Stone/Stone/Channel.swift
@@ -16,9 +16,13 @@ public enum ChannelState {
 public final class Channel: Hashable, Equatable {
 	public let topic: String
 
-	public typealias Callback = Result<Void> -> Void
+	public typealias Callback = (result: Result<Message>) -> Void
 	public private(set) var state: ChannelState = .Closed
-	private var bindings = [String: Callback]()
+
+	private var eventBindings = [Event: Callback]()
+	private var callbackBindings = [Event: Callback]()
+
+	private weak var socket: Socket?
 
 	public var hashValue: Int {
 		return topic.hashValue
@@ -30,29 +34,76 @@ public final class Channel: Hashable, Equatable {
 
 	public init(socket: Socket, topic: String) {
 		self.topic = topic
+		self.socket = socket
 	}
 
 	public func isMemberOfTopic(otherTopic: String) -> Bool {
 		return topic == otherTopic
 	}
 
-	internal func sendMessage(message: Message) {
+	internal func triggerEvent(event: Event, ref: String? = nil, payload: [String: AnyObject] = [:]) {
+		guard let callback = eventBindings[event] else {
+			return
+		}
+
+		let message = Message(
+			topic: topic,
+			event: event,
+			payload: payload,
+			ref: ref
+		)
+
+		if event == Event.PhoenixEvent.Reply {
+			receivedReply(message)
+		} else {
+			callback(result: .Success(message))
+		}
+	}
+
+	private func receivedReply(message: Message) {
+		print("Received reply in channel for message: \(message)")
+
+		guard let callback = callbackBindings[message.event] else {
+			return
+		}
+
+		callback(result: .Success(message))
+	}
+
+	public func onEvent(event: Event, callback: Callback) -> Callback? {
+		return eventBindings.updateValue(callback, forKey: event)
+	}
+
+	public func offEvent(event: Event) -> Callback? {
+		return eventBindings.removeValueForKey(event)
+	}
+
+	public func sendMessage(message: Message, completion: Callback) {
+		guard let socket = socket else {
+			completion(result: .Failure(Error.LostSocket))
+			return
+		}
+
+		callbackBindings.updateValue(completion, forKey: message.event)
+
+		do {
+			try socket.push(message)
+		} catch {
+			completion(result: .Failure(Error.InvalidJSON))
+		}
+	}
+
+	public func join(completion: Callback) {
+		let joinMessage = Message(
+			topic: topic,
+			event: Event.Default(.Join),
+			payload: [:]
+		)
 		
-	}
-
-	internal func triggerEvent(event: Event, ref: String, payload: [String: AnyObject]? = nil) {
-
-	}
-
-	public func onEvent<RawType: RawRepresentable where RawType.RawValue == String>(event: RawType, callback: Callback) {
-		onEvent(event.rawValue, callback: callback)
-	}
-
-	public func onEvent(event: String, callback: Callback) {
-		
+		sendMessage(joinMessage, completion: completion)
 	}
 }
 
 public func == (lhs: Channel, rhs: Channel) -> Bool {
-	return true
+	return lhs.hashValue == rhs.hashValue
 }
