0c8aa30833e53a83b0131a85600f41136b80d689
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-22 01:04:29 -0400

    Added Presence (not implemented yet)

---
+++ Stone/Presence.swift
@@ -0,0 +1,13 @@
+//
+//  Presence.swift
+//  Stone
+//
+//  Created by Michael MacCallum on 5/21/16.
+//  Copyright © 2016 Tethr Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+public class Presence {
+
+}
