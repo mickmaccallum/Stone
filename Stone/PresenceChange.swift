37e9907113c67296c9dfab899e8c42cbcafd4142
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-23 02:27:08 -0400

    PresenceChange

---
+++ Stone/PresenceChange.swift
@@ -0,0 +1,14 @@
+//
+//  PresenceChange.swift
+//  Stone
+//
+//  Created by Michael MacCallum on 5/23/16.
+//  Copyright © 2016 Tethr Technologies Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct PresenceChange {
+	let name: String
+	let metas: [String: AnyObject]
+}
