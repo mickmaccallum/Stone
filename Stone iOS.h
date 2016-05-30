6e0c8b7904e05c3a931997199156b5b43c80232f
Michael MacCallum <micksmaccallum@gmail.com>
2016-05-29 20:18:20 -0400

    Added Carthage support

---
+++ Stone iOS.h
@@ -0,0 +1,19 @@
+//
+//  Stone iOS.h
+//  Stone iOS
+//
+//  Created by Michael MacCallum on 5/29/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stone iOS.
+FOUNDATION_EXPORT double Stone_iOSVersionNumber;
+
+//! Project version string for Stone iOS.
+FOUNDATION_EXPORT const unsigned char Stone_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stone_iOS/PublicHeader.h>
+
+
