a6d6924bc87805a22f6878eced48fa6f0183e7a1
Michael MacCallum <micksmaccallum@gmail.com>
2016-07-21 19:48:32 -0400

    bump

--- Stone iOS.h
+++ Stone iOS.h
@@ -9,10 +9,10 @@
 #import <UIKit/UIKit.h>
 
 //! Project version number for Stone iOS.
-FOUNDATION_EXPORT double Stone_iOSVersionNumber;
+FOUNDATION_EXPORT double StoneVersionNumber;
 
 //! Project version string for Stone iOS.
-FOUNDATION_EXPORT const unsigned char Stone_iOSVersionString[];
+FOUNDATION_EXPORT const unsigned char StoneVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Stone_iOS/PublicHeader.h>
 
