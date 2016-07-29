ba7dbe6bdbe827ea199e9033e828e62cc2ecd704
Michael MacCallum <micksmaccallum@gmail.com>
2016-07-29 03:27:04 -0400

    added fallback for different payload nesting levels

--- Sources/Message.swift
+++ Sources/Message.swift
@@ -33,7 +33,7 @@ public struct Message {
 extension Message: Unboxable {
 	public init(unboxer: Unboxer) {
 		topic		= unboxer.unbox("topic")
-		payload		= unboxer.unbox("payload.response")
+		payload		= unboxer.unbox("payload.response") ?? unboxer.unbox("payload")
 		ref			= unboxer.unbox("ref")
 
 		let eventString: String = unboxer.unbox("event")
